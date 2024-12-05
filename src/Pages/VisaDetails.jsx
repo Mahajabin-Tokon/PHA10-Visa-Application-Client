83845f778c072b113f2e451945f537b13ae618c2
Mahajabin Tokon <tokonislam1122@gmail.com>
2024-12-05 09:19:21 -0700

    Applied button modal complete

--- src/Pages/VisaDetails.jsx
+++ src/Pages/VisaDetails.jsx
@@ -1,9 +1,49 @@
-import React from "react";
+import React, { useContext } from "react";
 import { useLoaderData } from "react-router-dom";
+import { authContext } from "../AuthProvider/AuthProvider";
+import Swal from "sweetalert2";
 
 const VisaDetails = () => {
+  const { user } = useContext(authContext);
   const data = useLoaderData();
   const { _id } = data;
+
+  const handleApplyModal = () => {
+    document.getElementById("applyModal").showModal();
+  };
+
+  const handleApply = () => {
+    event.preventDefault();
+    document.getElementById("applyModal").close();
+    const form = event.target;
+    const email = form.email.value;
+    const firstName = form.firstName.value;
+    const lastName = form.lastName.value;
+    const date = form.date.value;
+    const fee = form.fee.value;
+    const appliedInfo = { email, firstName, lastName, date, fee };
+    console.log(appliedInfo);
+
+    fetch(`http://localhost:5001/myAppliedVisas/${_id}`, {
+      method: "PUT",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(appliedInfo),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        if (data.modifiedCount > 0) {
+          Swal.fire({
+            title: "Success!",
+            text: "Visa Applied Successfully!",
+            icon: "success",
+            confirmButtonText: "Cool",
+          });
+        }
+      });
+  };
 
   return (
     <div className="max-w-6xl mx-auto my-2 px-2 py-10">
@@ -15,7 +55,9 @@ const VisaDetails = () => {
           <h2 className="card-title">{data.countryName}</h2>
           <p>{data.visaType}</p>
           <p>{data.processingTime}</p>
-          {data.requiredDoc.map((eachData) => <p>{eachData}</p>)}
+          {data.requiredDoc.map((eachData) => (
+            <p>{eachData}</p>
+          ))}
           <p>{data.description}</p>
           <p>{data.age}</p>
           <p>{data.fee}</p>
@@ -23,10 +65,100 @@ const VisaDetails = () => {
           <p>{data.applicationMethod}</p>
           <p>{data.applied}</p>
           <div className="card-actions justify-end">
-            <button className="btn">Apply for Visa</button>
+            <button onClick={handleApplyModal} className="btn">
+              Apply for Visa
+            </button>
           </div>
         </div>
       </div>
+
+      {/* Apply Button Modal */}
+      <dialog id="applyModal" className="modal">
+        <div className="modal-box w-full">
+          <form
+            className="w-full mx-auto bg-[#F4F3F0] rounded-lg py-10"
+            onSubmit={handleApply}
+          >
+            {/* Field 1 */}
+            <div className="px-10 py-2">
+              <div className="w-full">
+                <p>Email</p>
+                <input
+                  defaultValue={user?.email}
+                  name="email"
+                  type="text"
+                  placeholder="Email"
+                  className="input input-bordered w-full"
+                />
+              </div>
+            </div>
+            {/* Field 2 */}
+            <div className="px-10 py-2">
+              <div className="w-full">
+                <p>First Name</p>
+                <input
+                  name="firstName"
+                  type="text"
+                  placeholder="First Name"
+                  className="input input-bordered w-full"
+                />
+              </div>
+            </div>
+            {/* Field 3 */}
+            <div className="px-10 py-2">
+              <div className="w-full">
+                <p>Last Name</p>
+                <input
+                  name="lastName"
+                  type="text"
+                  placeholder="Last Name"
+                  className="input input-bordered w-full"
+                />
+              </div>
+            </div>
+            {/* Field 4 */}
+            <div className="px-10 py-2">
+              <div className="w-full">
+                <p>Applied Date</p>
+                <input
+                  defaultValue={new Date().toISOString().substr(0, 10)}
+                  name="date"
+                  type="date"
+                  placeholder="Applied Date"
+                  className="input input-bordered w-full"
+                />
+              </div>
+            </div>
+            {/* Field 5 */}
+            <div className="px-10 py-2">
+              <div className="w-full">
+                <p>Fee</p>
+                <input
+                  defaultValue={data?.fee}
+                  name="fee"
+                  type="text"
+                  placeholder="Fee"
+                  className="input input-bordered w-full"
+                />
+              </div>
+            </div>
+
+            <div className="px-10 py-4">
+              <input
+                type="submit"
+                value="Apply"
+                className="btn w-full bg-cyan-300"
+              />
+            </div>
+          </form>
+          <div className="modal-action">
+            <form method="dialog">
+              {/* if there is a button in form, it will close the modal */}
+              <button className="btn">Close</button>
+            </form>
+          </div>
+        </div>
+      </dialog>
     </div>
   );
 };
