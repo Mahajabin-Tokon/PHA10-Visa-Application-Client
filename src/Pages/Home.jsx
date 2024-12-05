92712b78aee4aaf7c06237c8ede0e8718929682d
Mahajabin Tokon <tokonislam1122@gmail.com>
2024-12-05 13:55:01 -0700

    React typewriter used

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -4,13 +4,18 @@ import Feedback from "../Components/Feedback";
 import Success from "../Components/Success";
 import LatestVisa from "../Components/LatestVisa";
 import { authContext } from "../AuthProvider/AuthProvider";
+import { useTypewriter } from "react-simple-typewriter";
 
 const Home = () => {
   const { isDark } = useContext(authContext);
-  console.log(isDark);
+  const [text] = useTypewriter({
+    words: ["Best", "Visa", "Application site!"],
+    loop: 0,
+  });
   return (
     <div className={`${isDark ? "bg-black" : ""}`}>
       <div className="max-w-6xl mx-auto my-2 px-2">
+        <div className="text-center text-4xl py-10">Welcome to {text}</div>
         <Banner></Banner>
       </div>
       <div className="max-w-6xl mx-auto my-2 px-2 py-10">
