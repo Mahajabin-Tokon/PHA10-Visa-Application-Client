36d05528b7243da5c4f20ba3700a0932d6190384
Mahajabin Tokon <tokonislam1122@gmail.com>
2024-12-04 19:12:42 -0700

    Delete functionality added

--- src/Pages/MyAddedVisas.jsx
+++ src/Pages/MyAddedVisas.jsx
@@ -1,15 +1,50 @@
 import React, { useContext, useEffect, useState } from "react";
 import { authContext } from "../AuthProvider/AuthProvider";
 import { Link } from "react-router-dom";
+import Swal from "sweetalert2";
 
 const MyAddedVisas = () => {
   const { user } = useContext(authContext);
   const [visas, setVisas] = useState();
+//   const { _id } = visas;
   useEffect(() => {
     fetch(`http://localhost:5001/myAddedVisas?email=${user.email}`)
       .then((res) => res.json())
       .then((data) => setVisas(data));
   }, []);
+
+  const handleDelete = (_id) => {
+    // console.log(_id);
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        fetch(`http://localhost:5001/myAddedVisas/${_id}`, {
+          method: "DELETE",
+        })
+          .then((res) => res.json())
+          .then((data) => {
+            console.log(data);
+            if (data.deletedCount > 0) {
+              Swal.fire({
+                title: "Deleted!",
+                text: "Your coffee has been deleted.",
+                icon: "success",
+              });
+              const remaining = visas.filter(visa => visa._id != _id)
+              setVisas(remaining)
+            }
+          });
+      }
+    });
+  };
+
   return (
     <div className="max-w-6xl mx-auto my-2 px-2">
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 py-5 ">
@@ -27,7 +62,15 @@ const MyAddedVisas = () => {
               <p>{eachVisa.applicationMethod}</p>
               <div className="card-actions justify-end">
                 <Link to={`/visaDetails/${eachVisa._id}`} className="btn">
-                  See Detials
+                  Update
+                </Link>
+                <Link
+                  onClick={() => {
+                    handleDelete(eachVisa._id);
+                  }}
+                  className="btn"
+                >
+                  Delete
                 </Link>
               </div>
             </div>
