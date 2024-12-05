dacdcbe2a10dc65268c0131155141d2c0450bcc5
Mahajabin Tokon <tokonislam1122@gmail.com>
2024-12-04 18:23:59 -0700

    My added visas page added

--- src/Pages/MyAddedVisas.jsx
+++ src/Pages/MyAddedVisas.jsx
@@ -1,11 +1,14 @@
-import React from 'react';
+import React, { useContext, useEffect } from "react";
+import { authContext } from "../AuthProvider/AuthProvider";
 
 const MyAddedVisas = () => {
-    return (
-        <div>
-            MyAddedVisas
-        </div>
-    );
+  const { user } = useContext(authContext);
+  useEffect(() => {
+    fetch(`http://localhost:5001/myAddedVisas?email=${user.email}`,)
+    .then((res) => res.json())
+    .then((data) => console.log(data));
+  }, []);
+  return <div>MyAddedVisas</div>;
 };
 
 export default MyAddedVisas;
